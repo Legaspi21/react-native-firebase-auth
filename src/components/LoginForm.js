4f35383bee4c01e758742ad36efbb9f6cbca1feb
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-02-22 12:53:21 -0800

    Adds Inputs to handle email and password entry with secure password text entry

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-	state = { text: '' };
+	state = { email: '', password: '' };
 
 	render() {
 		return (
 			<Card>
 				<CardSection>
-					<TextInput 
-						value={this.state.text}
-						onChangeText={text => this.setState({ text })}
-						style={{ height: 20, width: 100 }}
+					<Input
+						label="Email"
+						placeholder="user@gmail.com"
+						value={this.state.email}
+						onChangeText={email => this.setState({ email })}
 					/>
 				</CardSection>
 
-				<CardSection />
+				<CardSection>
+					<Input 
+						label="Password"
+						placeholder="password"
+						secureTextEntry
+						value={this.state.password}
+						onChangeText={password => this.setState({ password })}
+					/>
+				</CardSection>
 				
 				<CardSection>
 					<Button>
