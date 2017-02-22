8dc624389b72761a401eafbbfe8b4ce0b20ab449
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-02-22 12:08:46 -0800

    Firebase initialization

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp({
+    apiKey: 'AIzaSyB_xIlM29RBBrV4a6Ni5ovYK_gRgbfcuX0',
+    authDomain: 'react-native-auth-e5062.firebaseapp.com',
+    databaseURL: 'https://react-native-auth-e5062.firebaseio.com',
+    storageBucket: 'react-native-auth-e5062.appspot.com',
+    messagingSenderId: '589203064119'
+		});
+	}
+
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<Text>App</Text>
+				<LoginForm />
 			</View>
 		);
 	}
